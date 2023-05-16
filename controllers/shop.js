a6cd9ff755dcb3968aeb14e8a5e150402d7bbd4f
mac <mac@MacBook-Air-mac.local>
2023-05-16 13:19:34 +0300

    implement sequelize

---
+++ controllers/shop.js
@@ -0,0 +1,121 @@
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.getProducts = (req, res, next) => {
+	Product.findAll()
+		.then((products) => {
+			res.render("shop/product-list", {
+				prods: products,
+				pageTitle: "All Products",
+				path: "/products",
+			});
+		})
+		.catch((err) => console.log(err));
+	// Product.fetchAll()
+	// 	.then(([rows, fieldContent]) =>
+	// 		res.render("shop/product-list", {
+	// 			prods: rows,
+	// 			pageTitle: "All Products",
+	// 			path: "/products",
+	// 		})
+	// 	)
+	// 	.catch((err) => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId;
+	Product.findByPk(prodId)
+		.then((productById) => {
+			res.render("shop/product-detail", {
+				product: productById,
+				pageTitle: productById.title,
+				path: "/products",
+			});
+		})
+		.catch((err) => console.log(err));
+	// Product.findById(prodId)
+	// 	.then(([product]) => {
+	// 		console.log(product);
+	// 		res.render("shop/product-detail", {
+	// 			product: product[0],
+	// 			pageTitle: product.title,
+	// 			path: "/products",
+	// 		});
+	// 	})
+	// 	.catch((err) => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+	Product.findAll()
+		.then((products) => {
+			res.render("shop/index", {
+				prods: products,
+				pageTitle: "Shop",
+				path: "/",
+			});
+		})
+		.catch((err) => console.log(err));
+	// Product.fetchAll()
+	// 	.then(([rows, fieldContent]) =>
+	// 		res.render("shop/index", {
+	// 			prods: rows,
+	// 			pageTitle: "Shop",
+	// 			path: "/",
+	// 		})
+	// 	)
+	// 	.catch((err) => console.log(err));
+};
+
+exports.getCart = (req, res, next) => {
+	Cart.getCart((cart) => {
+		Product.fetchAll((products) => {
+			const cartProducts = [];
+			for (product of products) {
+				const cartProductData = cart.products.find(
+					(prod) => prod.id === product.id
+				);
+				if (cartProductData) {
+					cartProducts.push({
+						productData: product,
+						qty: cartProductData.qty,
+					});
+				}
+			}
+			res.render("shop/cart", {
+				path: "/cart",
+				pageTitle: "Your Cart",
+				products: cartProducts,
+			});
+		});
+	});
+};
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.addProduct(prodId, product.price);
+	});
+	res.redirect("/cart");
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.deleteProduct(prodId, product.price);
+		res.redirect("/cart");
+	});
+};
+
+exports.getOrders = (req, res, next) => {
+	res.render("shop/orders", {
+		path: "/orders",
+		pageTitle: "Your Orders",
+	});
+};
+
+exports.getCheckout = (req, res, next) => {
+	res.render("shop/checkout", {
+		path: "/checkout",
+		pageTitle: "Checkout",
+	});
+};
