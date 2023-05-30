26418af0262da468a91c78bfbea5827c8706227d
mac <mac@MacBook-Air-mac.local>
2023-05-30 18:03:15 +0300

    add mongo DB

--- models/product.js
+++ models/product.js
@@ -79,30 +79,59 @@
 // 	}
 // };
 
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-const sequelize = require("../util/database");
+// const sequelize = require("../util/database");
 
-const Product = sequelize.define("product", {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
-	title: Sequelize.STRING,
-	price: {
-		type: Sequelize.DOUBLE,
-		allowNull: false,
-	},
-	imageUrl: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	description: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-});
-
+// const Product = sequelize.define("product", {
+// 	id: {
+// 		type: Sequelize.INTEGER,
+// 		autoIncrement: true,
+// 		allowNull: false,
+// 		primaryKey: true,
+// 	},
+// 	title: Sequelize.STRING,
+// 	price: {
+// 		type: Sequelize.DOUBLE,
+// 		allowNull: false,
+// 	},
+// 	imageUrl: {
+// 		type: Sequelize.STRING,
+// 		allowNull: false,
+// 	},
+// 	description: {
+// 		type: Sequelize.STRING,
+// 		allowNull: false,
+// 	},
+// });
+const getDB = require("../util/database").getDB;
+class Product {
+	constructor(title, price, description, imageUrl) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.imageUrl = imageUrl;
+	}
+	save() {
+		const db = getDB();
+		return db
+			.collection("products")
+			.insertOne(this)
+			.then((result) => {
+				console.log(result);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
+	static fetchAll() {
+		const db = getDB();
+		return db
+			.collection("products")
+			.find()
+			.toArray()
+			.then((products) => products)
+			.catch((err) => console.log(err, "s"));
+	}
+}
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,7 +1,23 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node_complete", "root", "DonnaLee23", {
-	dialect: "mysql",
-	host: "localhost",
-});
-module.exports = sequelize;
+let _db;
+const mongoConnect = (cb) => {
+	MongoClient.connect(
+		"mongodb+srv://sashamakarov28:sashamakarov2823@cluster0.6ux7xyh.mongodb.net/shop?retryWrites=true&w=majority"
+	)
+		.then((client) => {
+			console.log("Connected!");
+			_db = client.db();
+			cb();
+		})
+		.catch((err) => console.log(err));
+};
+const getDB = () => {
+	if (_db) {
+		return _db;
+	}
+	throw "no database";
+};
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
